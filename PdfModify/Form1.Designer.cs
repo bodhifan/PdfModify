2fdcc800224bd6bb39712c760ae14c011ce8ac80
bodhifan <bodhifan@163.com>
2016-08-16 20:11:58 +0800

    初始工程
    修改pdf

---
+++ PdfModify/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PdfModify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(446, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(446, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(446, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "10";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "123";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(55, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "567";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(55, 238);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "445";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(188, 45);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(240, 253);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(55, 277);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.Text = "20";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 323);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ PdfModify/Form1.cs
@@ -0,0 +1,106 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PdfModify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RemoveText(oldFile,newFile, "惠州大亚湾西区新寮新村三巷11号龙泉花园3栋2004号房");
+            MessageBox.Show("finished");
+        }
+        string oldFile = @"E:\\zzz.pdf";
+        string newFile = @"E:\\new.pdf";
+        string fontFile = @"C:\Program Files (x86)\Adobe\Acrobat Reader DC\Resource\CIDFont\AdobeSongStd-Light.otf";
+        public void RemoveText(string srcFile,string targetFile,string text)
+        {
+            // 输出文件pdf
+            string outputFilePath = targetFile;
+            // 输入文件pdf
+            string inputFilePath = srcFile;
+            try
+            {
+                using (Stream inputPdfStream = new FileStream(inputFilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (Stream outputPdfStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                using (Stream outputPdfStream2 = new FileStream(outputFilePath, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                {
+                    // 打开原始pdf进行读操作
+                    PdfReader reader = new PdfReader(inputPdfStream);
+                    
+                    // 创建一个stamper对象，进行写操作
+                    PdfStamper stamper = new PdfStamper(reader, outputPdfStream); //{ FormFlattening = true, FreeTextFlattening = true };
+
+                    // 1. 以图片覆盖需要写入的地方“坐落”
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(new Bitmap(Convert.ToInt32(textBox4.Text), Convert.ToInt32(textBox6.Text)), BaseColor.WHITE);
+                    
+                    // “坐落”一栏的位置
+                    image.SetAbsolutePosition(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+                    
+                    // 将图像添加到第二页
+                    var contentByte = stamper.GetOverContent(2);
+                    stamper.GetOverContent(2).AddImage(image, true);
+                   
+                    // 2. 在被图像覆盖的地方写入目标文字
+                     var baseFont = BaseFont.CreateFont(fontFile, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+                    contentByte.BeginText();
+                    contentByte.SetFontAndSize(baseFont, Convert.ToInt32(textBox1.Text));
+
+                    SizeF size = GetLenOfString(text);
+
+                    textBox5.Text += "width: " + size.Width + "\theight:"+size.Height+ "\r\n";
+
+                    float X = Convert.ToInt32(textBox2.Text) + (Convert.ToInt32(textBox4.Text) - size.Width) / 2;
+
+                    float centerY = (20 - size.Height) / 2.0f;
+                    textBox5.Text += "centerY: " + centerY + "\r\n";
+                    float Y = Convert.ToInt32(textBox3.Text) + centerY*2;
+                    textBox5.Text += "startX: " + X + "\r\n";
+                    textBox5.Text += "startY: " + Y + "\r\n";
+
+                    contentByte.ShowTextAligned(PdfContentByte.ALIGN_LEFT, text,X, Y, 0);
+
+                    contentByte.EndText();
+
+                    stamper.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private SizeF GetLenOfString(string multiLineString)
+        {
+            using (System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(new Bitmap(1, 1)))
+            {
+                PrivateFontCollection pfc = new PrivateFontCollection();
+                pfc.AddFontFile(@"C:\Windows\Fonts\STSONG.TTF");
+                System.Drawing.Font font = new System.Drawing.Font(pfc.Families[0], 10, FontStyle.Regular,GraphicsUnit.Pixel);
+                SizeF size = graphics.MeasureString(multiLineString, font);
+                return size;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
